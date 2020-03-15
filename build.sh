373069ff4c1b56e6eeb1ca27641c2dc1b73d13b0
wulan17 <galihgustip@gmail.com>
2020-03-15 13:10:18 +0700

    builds: fix up
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- build.sh
+++ build.sh
@@ -23,23 +23,23 @@ else
 fi
 
 function sync(){
-	curl -F "chat_id=$chat" -F "parse_mode=html" -F "text=Sync Started" https://api.telegram.org/bot$telegram/sendMessage > /dev/null
+	curl -F "chat_id=$chat" -F "parse_mode=html" -F "text=Sync Started" https://api.telegram.org/bot"$telegram"/sendMessage > /dev/null
 	SYNC_START=$(date +"%s")
 	repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 	SYNC_END=$(date +"%s")
 	SYNC_DIFF=$((SYNC_END - SYNC_START))
-	curl -F "chat_id=$chat" -F "parse_mode=html" -F "text=Sync completed in $(((SYNC_DIFF / 60) / 60)):$(((SYNC_DIFF / 60) % 60)):$((SYNC_DIFF % 60))" https://api.telegram.org/bot$telegram/sendMessage > /dev/null
+	curl -F "chat_id=$chat" -F "parse_mode=html" -F "text=Sync completed in $(((SYNC_DIFF / 60) / 60)):$(((SYNC_DIFF / 60) % 60)):$((SYNC_DIFF % 60))" https://api.telegram.org/bot"$telegram"/sendMessage > /dev/null
 }
 function clean(){
-	curl -F "chat_id=$chat" -F "parse_mode=html" -F "text=Cleaning old builds..." https://api.telegram.org/bot$telegram/sendMessage > /dev/null
+	curl -F "chat_id=$chat" -F "parse_mode=html" -F "text=Cleaning old builds..." https://api.telegram.org/bot"$telegram"/sendMessage > /dev/null
 	make clean -j$(nproc --all)
 }
 function check_old(){
 	if [[ -z $(find $outdir/*2020*.zip | cut -d "/" -f 5) ]]; then
-		last_build=$(ls $outdir/*2020*.zip | tail -n -1)
-		export last_md5=$(md5sum $last_build | cut -d ' ' -f 1)
-	else
 		export last_md5=0
+	else
+		last_build=$(ls $outdir/*2020*.zip | tail -n -1)
+		export last_md5=$(md5sum "$last_build" | cut -d ' ' -f 1)
 	fi
 }
 function success(){
@@ -47,19 +47,19 @@ function success(){
 		wget https://github.com/wulan17/builds/raw/master/github-release
 		chmod +x github-release
 	fi
-	./github-release "$release_repo" "$tag" "master" ""$ROM" for "$device"
+	./github-release "$release_repo" "$tag" "master" """$ROM"" for ""$device""
 	Date: $(env TZ="$timezone" date)" "$file_path"
-	curl -F "chat_id=$chat" -F "parse_mode=html" -F "text=Build completed successfully in $(((BUILD_DIFF / 60) / 60)):$(((BUILD_DIFF / 60) % 60)):$((BUILD_DIFF % 60))<br/>Download: <a href='https://github.com/$release_repo/releases/download/$tag/$zip_name'>$zip_name</a>" https://api.telegram.org/bot$telegram/sendMessage > /dev/null
+	curl -F "chat_id=$chat" -F "parse_mode=html" -F "text=Build completed successfully in $(((BUILD_DIFF / 60) / 60)):$(((BUILD_DIFF / 60) % 60)):$((BUILD_DIFF % 60))<br/>Download: <a href='https://github.com/$release_repo/releases/download/$tag/$zip_name'>$zip_name</a>" https://api.telegram.org/bot"$telegram"/sendMessage > /dev/null
 }
 function failed(){
-	curl -F "chat_id=$chat" -F document=@$HOME/log-$(echo $vendor).txt -F "parse_mode=html" -F "caption=Build failed in $(((BUILD_DIFF / 60) / 60)):$(((BUILD_DIFF / 60) % 60)):$((BUILD_DIFF % 60))" https://api.telegram.org/bot$telegram/sendDocument > /dev/null
+	curl -F "chat_id=$chat" -F document=@"$HOME"/log-$(echo "$vendor").txt -F "parse_mode=html" -F "caption=Build failed in $(((BUILD_DIFF / 60) / 60)):$(((BUILD_DIFF / 60) % 60)):$((BUILD_DIFF % 60))" https://api.telegram.org/bot"$telegram"/sendDocument > /dev/null
 }
 function check_build(){
-	if [[ -z $(find $outdir/*2020*.zip | cut -d "/" -f 5) ]]; then
+	if [[ -e $(find $outdir/*2020*.zip | cut -d "/" -f 5) ]]; then
 		export file_path=$(ls $outdir/*2020*.zip | tail -n -1)
-		export file_name=$(echo $file_path | cut -d "/" -f 5)
-		md5=$(md5sum $file_path | cut -d ' ' -f 1)
-		if [ $md5 == $last_md5 ]; then
+		export file_name=$(echo "$file_path" | cut -d "/" -f 5)
+		md5=$(md5sum "$file_path" | cut -d ' ' -f 1)
+		if [ "$md5" == "$last_md5" ]; then
 			failed
 		else
 			export tag=$md5
@@ -70,22 +70,22 @@ function check_build(){
 	fi
 }
 function build(){
-	curl -F "chat_id=$chat" -F "parse_mode=html" -F "text=Build Started For <a href='$repo'>$rom $branch</a><br />Device : $device" https://api.telegram.org/bot$telegram/sendMessage > /dev/null
+	curl -F "chat_id=$chat" -F "parse_mode=html" -F "text=Build Started For <a href='$repo'>$rom $branch</a><br />Device : $device" https://api.telegram.org/bot"$telegram"/sendMessage > /dev/null
 	BUILD_START=$(date +"%s")
 	source build/envsetup.sh
 	#breakfast $device
-	lunch $vendor_$device-userdebug
+	lunch "$vendor"_"$device"-userdebug
 	#brunch $device > ~/log-$vendor.txt
 	#make aex -j$(nproc --all) > ~/log-$vendor.txt
 	#make otapackage -j$(nproc --all) > ~/log-$vendor.txt
 	#mka kronic -j$(nproc --all) > ~/log-$vendor.txt
-	mka bacon -j$(nproc --all) > ~/log-$vendor.txt
+	mka bacon -j$(nproc --all) > ~/log-"$vendor".txt
 	BUILD_END=$(date +"%s")
 	export BUILD_DIFF=$((BUILD_END - BUILD_START))
 }
 
 function main(){
-	curl -F "chat_id=$chat" -F "sticker=$sticker" https://api.telegram.org/bot$telegram/sendSticker > /dev/null
+	curl -F "chat_id=$chat" -F "sticker=$sticker" https://api.telegram.org/bot"$telegram"/sendSticker > /dev/null
 	if [ $SYNC==true ]; then
 		sync
 	fi
